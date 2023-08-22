ba62a72e71f12fe732a30a43b706785f2a49cbd7
Sean Stocker <sean.stocker14@gmail.com>
2023-08-22 20:14:16 +0100

    feat: implemented logic to create a post

--- src/components/AddPostModal/AddPostModal.js
+++ src/components/AddPostModal/AddPostModal.js
@@ -1,16 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
+import { useMutation, gql } from "@apollo/client";
+
+const CREATE_POST = gql`
+  mutation CreatePost($title: String, $content: String) {
+    postCreate(post: { title: $title, content: $content }) {
+      userErrors {
+        message
+      }
+      post {
+        title
+        content
+      }
+    }
+  }
+`;
 
 export default function AddPostModal() {
   const [show, setShow] = useState(false);
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+  const [error, setError] = useState(null);
+  const [createPost, { data, loading }] = useMutation(CREATE_POST);
+
+  useEffect(() => {
+    console.log(data);
+    if (data) {
+      if (data?.postCreate?.userErrors?.length > 0) {
+        setError(data.postCreate.userErrors[0].message);
+      } else {
+        window.location.reload();
+      }
+    }
+  }, [data]);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const [content, setContent] = useState("");
-  const [title, setTitle] = useState("");
+  const handleClick = () => {
+    if (!title || !content) {
+      setError("Please fill out all fields");
+      return;
+    }
+    createPost({
+      variables: {
+        title: title,
+        content: content,
+      },
+    });
+    handleClose();
+  };
 
-  const handleClick = () => {};
+  if (loading) return <p>Loading...</p>;
 
   return (
     <>
@@ -54,6 +95,7 @@ export default function AddPostModal() {
           </Form>
         </Modal.Body>
         <Modal.Footer>
+          {error && <p>{error}</p>}
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
